3b048218048b83f7ffe3046ca8281711ec88bdd4
Martin Norberg <martin.norberg@live.com>
2015-09-24 01:58:45 +0200

    no message

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SquirrelJump
-Puzzle
+
+### Problem:
+Find the highest number of jumps a squirrel can make in a bar graph. The bar graph is represented by a vector of integer values.
+
+Rules:
+- You choose on which bar the squirrel starts
+- The squirrel may only jump to a lower bar height
+- The squirrel can jump a maximum of two bars away
+
+Example:
+- Bars: [6, 4, 14, 6, 8, 13, 9, 7, 10, 6, 12]
+- Solution: The longest jump sequence is five jumps long: From 12 to 10 to 9 to 8 to 6 to 4.
+
+### Solution:
+This solution uses a recursive function, that takes an index to a bar, which will call for itself as long as there is a possible jump to another bar. The function returns the maximal depth.
---
+++ main.cpp
@@ -0,0 +1,60 @@
+//
+//  main.cpp
+//  Squirrel
+//
+//  Created by Martin Olof Norberg on 2015-09-23.
+//  Copyright © 2015 Martin Norberg. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int recursive(const int& index, const vector<int>& my_array) {
+    vector<int> v;
+    int max_jumps = 0;
+    string level(depth, ' ');
+
+    if (index > 0)
+    {
+        if (my_array.at(index-1) < my_array.at(index))
+            v.push_back(index-1);
+        if (index > 1 && my_array.at(index-2) < my_array.at(index))
+            v.push_back(index-2);
+    }
+    if (index < my_array.size()-1)
+    {
+        if (my_array.at(index+1) < my_array.at(index))
+            v.push_back(index+1);
+        if (index < my_array.size()-2 && my_array.at(index+2) < my_array.at(index))
+            v.push_back(index+2);
+    }
+    
+    if (v.size() > 0)
+    {
+        ++max_jumps;
+        for (int index : v)
+        {
+            int jumps = recursive(index, my_array, depth+5);
+            if (jumps >= max_jumps)
+                max_jumps = jumps+1;
+        }
+    }
+    
+    return max_jumps;
+}
+
+
+int main() {
+    
+    vector<int> my_array = {6, 4, 14, 6, 8, 13, 9, 7, 10, 6, 12};
+    int max_jumps = 0;
+    
+    for (int index=0; index<my_array.size(); ++index)
+        max_jumps = recursive(index, my_array);
+    
+    cout << "Max jumps: " << max_jumps << endl;
+    
+    return 0;
+}
